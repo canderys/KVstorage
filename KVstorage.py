79319d5d9f9742f81f29c0d2cef62d1d1604b0cf
ui.chaykov <ui.chaykov@tensor.ru>
2019-11-22 14:54:54 +0500

    начал делать MemoryStorage

--- KVstorage.py
+++ KVstorage.py
@@ -1,4 +1,7 @@
-class KVstorage():
+import os
+
+
+class KVstorage:
 
     def __init__(self):
         self.dict = {}
@@ -14,3 +17,54 @@ class KVstorage():
 
     def keys(self):
         return self.dict.keys()
+
+
+class MemoryStorage:
+    def __init__(self, data=str()):
+        self.data = data
+
+    def __del__(self):
+        self._save()
+
+    def print(self):
+        print(self.data)
+
+    def append(self, key, value):
+        if not self._can_append_item():
+            self._save()
+            self.data = str()
+
+        self.data = self.data + '{} {} {}\n'.format(hash(str(key) + str(value)), key, value)
+
+
+    def get(self, key):
+        pass
+        # self.data[key]
+
+    def _can_append_item(self):
+        return True
+
+    def _save(self):
+        pass
+
+
+class FileStorage:
+    pass
+
+
+# class FileStorage:
+#     def __init__(self):
+#         file_name = 'file_storage.fs'
+#         if not os.path.exists(file_name):
+#             self.file = open(file_name, 'rw')
+#
+#     def __del__(self):
+#         if self.file:
+#             self.file.close()
+#
+#     def append(self, key, value):
+#         pass
+#
+#     def find(self, key):
+#         pass
+
